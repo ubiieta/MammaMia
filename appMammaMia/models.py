5a11393eba8d7ad0b2bb618df7b322e5afd8dc48
enekoalvarz <eneko.alvarez@opendeusto.es>
2024-11-14 18:19:45 +0100

    bd con datos funcional en pagina

--- appMammaMia/models.py
+++ appMammaMia/models.py
@@ -4,17 +4,32 @@ from django.db import models
 
  
 class Masa(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50, unique=True)
+    descripcion = models.TextField(blank=True, null=True)
+    precio_extra = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.nombre
+
+
 class Ingrediente(models.Model):
     nombre = models.CharField(max_length=50)
+    descripcion = models.TextField(blank=True, null=True)
+    es_vegetariano = models.BooleanField(default=True)
+    precio_extra = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.nombre
  
 class Pizza(models.Model):
     nombre = models.CharField(max_length=40)
+    descripcion = models.TextField(blank=True, null=True)
     precio = models.IntegerField()
-    
-    # Campo para la relación one-to-many (una pizza tiene solo una masa)
+    tiempo_preparacion = models.IntegerField(default=15)
+
     masa = models.ForeignKey(Masa, on_delete=models.CASCADE)
-    # Campo para la relación many-to-many (una pizza tiene varios ingredientes)
     ingredientes = models.ManyToManyField(Ingrediente)
     
+    def __str__(self):
+        return self.nombre
    
